21cdbae99fb81a3e302e059e1b7d629d47641cb1
rudra2802 <70274260+rudra2802@users.noreply.github.com>
2020-12-29 16:10:25 +0530

    Add files via upload

---
+++ Rows.js
@@ -0,0 +1,18 @@
+class Rows{
+    constructor(x,y,w,h){
+        var options = {
+            isStatic : true,
+        }
+        this.body = Bodies.rectangle(x,y,w,h,options);
+        this.w = w;
+        this.h = h;
+        this.color=color(random(0, 255), random(0, 255), random(0, 255));
+        World.add(world,this.body);
+    }
+    display(){
+    var pos = this.body.position;
+     rectMode(CENTER);
+     fill("yellow");
+    rect(pos.x,pos.y,this.w,this.h);
+    }
+};
